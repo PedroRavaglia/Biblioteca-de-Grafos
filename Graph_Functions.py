5db5d9407ac1a51162f7dac1d0ab91ad80ad3504
Pedro Ravaglia <74989519+PedroRavaglia@users.noreply.github.com>
2021-12-20 13:15:14 -0300

    Update Graph_Functions.py

--- Graph_Functions.py
+++ Graph_Functions.py
@@ -20,7 +20,7 @@ def read_graph(path, g_type):
         A = [] # Array que conterá as arestas do grafo
 
         # Percorrendo o restante das linhas do arquivo e adicionando em A
-        # as arrays que representam as arestas
+        # as listas que representam as arestas
         for line in f:
             a = line.split(' ')
             for i in range(2):
@@ -40,12 +40,12 @@ def read_graph(path, g_type):
             la = {} # Dicionário inicialmente vazio 
             for a in A:
                 if a[0] in la:
-                    la[a[0]].update([a[1]])
+                    la[a[0]].add(a[1])
                 else:
                     la[a[0]] = {a[1]}
 
                 if a[1] in la:
-                    la[a[1]].update([a[0]])
+                    la[a[1]].add(a[0])
                 else:
                     la[a[1]] = {a[0]}
             return la
@@ -54,6 +54,7 @@ def read_graph(path, g_type):
         else:
             print('\nTipo errado de representção do grafo. Escolher entre "ma" (matriz'
                 + 'de adjacência) ou "la" (lista de adjacência)')
+
 
 
 def num_edges(g):
@@ -77,6 +78,7 @@ def num_edges(g):
             for i in range(x, v_n):
                 a_n += l[i]
             x += 1
+        
 
     # Caso o grafo seja representado por uma lista de adjacência
     if isinstance(g, dict):
@@ -90,6 +92,7 @@ def num_edges(g):
                 edges[u-1].append(v)
 
     return a_n
+
 
 
 def min_degree(g):
@@ -116,12 +119,13 @@ def min_degree(g):
         d_min = min(degrees) 
 
     # Caso o grafo seja representado por uma lista de adjacência
-    if isinstance(g, dict):
+    elif isinstance(g, dict):
         for v in g:
             degrees.append(len(g[v]))
         d_min = min(degrees)
 
     return d_min
+
 
 
 def max_degree(g):
@@ -148,12 +152,13 @@ def max_degree(g):
         d_max = max(degrees)
 
     # Caso o grafo seja representado por uma lista de adjacência
-    if isinstance(g, dict):
+    elif isinstance(g, dict):
         for v in g:
             degrees.append(len(g[v]))
         d_max = max(degrees)
 
     return d_max
+
 
 
 def mean_degree(g):
@@ -180,12 +185,13 @@ def mean_degree(g):
             d = 0
 
     # Caso o grafo seja representado por uma lista de adjacência
-    if isinstance(g, dict):
+    elif isinstance(g, dict):
         for v in g:
             degrees.append(len(g[v]))
     
     mean = sum(degrees)/v_n
     return round(mean, 2)
+
 
 
 def median_degree(g):
@@ -212,7 +218,7 @@ def median_degree(g):
             d = 0
 
     # Caso o grafo seja representado por uma lista de adjacência
-    if isinstance(g, dict):
+    elif isinstance(g, dict):
         for v in g:
             degrees.append(len(g[v]))
 
@@ -224,6 +230,7 @@ def median_degree(g):
         median = (degrees[(v_n - 1)//2] + degrees[(v_n + 1)//2]) // 2
 
     return median
+
 
 
 def out_graph(g, path):
@@ -242,3 +249,149 @@ def out_graph(g, path):
         f.write('Grau máximo: ' + str(max_degree(g)) + '\n')
         f.write('Grau médio: ' + str(mean_degree(g)) + '\n')
         f.write('Mediana de grau: ' + str(median_degree(g)) + '\n')
+
+
+
+def BFS(g, v_1):
+    '''
+    Implementação do algoritmo de busca em largura
+    ------------------------------------------------------------------------------
+    ENTRADA:
+        - g (lista ou dicionário): grafo reprasentado por uma matriz de adjacência 
+        ou uma lista de adjacência
+        - v_1 (int): vértice inicial
+    ------------------------------------------------------------------------------
+    SAÍDA:
+        - explored (list): lista contendo os vértices do grafo na ordem em que foram
+        explorados (removidos da fila)
+    '''
+    visited = [0] * len(g) # Lista informando quais vértices já foram explorados
+    visited[v_1-1] = 1
+    explored = []
+    Q = [v_1]
+
+    # Caso o grafo seja representado por uma lista de adjacência
+    if isinstance(g, list):
+        while Q:
+            v = Q.pop(0)
+            for i in range(len(g)):
+                if g[v-1][i] == 1 and visited[i] == 0:
+                    visited[i] = 1
+                    Q.append(i+1)
+            explored.append(v) 
+
+    # Caso o grafo seja representado por uma lista de adjacência
+    elif isinstance(g, dict):
+        ...
+    
+    return explored
+
+
+
+def genTree_BFS(g, v_1, path):
+    '''
+    Cria um novo arquivo texto contendo a árvore gerada pelo algoritmo de busca em
+    largura (BFS) a partir do vértice v_1
+    ------------------------------------------------------------------------------
+    ENTRADA:
+        - g (lista ou dicionário): grafo reprasentado por uma matriz de adjacência 
+        ou uma lista de adjacência
+        - v_1 (int): vértice inicial
+        - path (string):
+    '''
+    visited = [0] * len(g) # Lista informando quais vértices já foram explorados
+    visited[v_1-1] = 1
+    levels = [None] * len(g) # Lista dos níveis de cada vértice da árvore gerada 
+    levels[v_1-1] = 0
+    prev = [None] * len(g) # Lista informando o pai de cada vértice
+    Q = [v_1]
+
+    layer = 0 # Atual camada na qual estamos buscando os seus vértices
+    cur_layer = [] # Lista em que serão adicionados os vértices correspondendo à camada atual
+    layers = [[v_1]] # Lista contendo os vértices agrupados pelas suas respectivas camadas
+                     # Ex: layers[i] é a lista dos vértices contidos na camada i
+
+    # Caso o grafo seja representado por uma lista de adjacência
+    if isinstance(g, list):
+        while Q:
+            v = Q.pop(0)
+            for i in range(len(g)):
+                # Encontrando os vértices filhos de v
+                if g[v-1][i] == 1 and visited[i] == 0:
+                    visited[i] = 1
+                    Q.append(i+1)
+                    prev[i] = v
+
+                # Determinando em que camada os véstices se encontram
+                for u in layers[layer]:
+                    if g[i][u-1] == 1 and levels[i] == None:
+                        levels[i] = layer + 1
+                        cur_layer.append(i+1)
+
+            # Depois teremos todos os vértices da camada atual contidos em cur_layer
+            layers.append(cur_layer)
+            # Caso a camada atual não seja vazia, incrementamos 'layer' para podermos encontrar os 
+            # vértices da próxima camada
+            if cur_layer:
+                layer += 1
+            cur_layer = []
+
+    # Caso o grafo seja representado por uma lista de adjacência
+    elif isinstance(g, dict):
+        ...
+
+    with open(path, 'w') as f:
+        f.write('Árvore gerada pelo algoritmo de busca em largura (BFS) a partir do vértice ' + str(v_1) + ': \n\n')
+
+        for i in range(layer+1):
+            for v in layers[i]:
+                f.write('Vértice: ' + str(v) + ', Pai: ' + str(prev[v-1]) + ', Nível: ' + str(levels[v-1]) + '\n')
+
+
+
+def DFS(g, v_1):
+    '''
+    Implementação do algoritmo de busca em profundidade
+    ------------------------------------------------------------------------------
+    ENTRADA:
+        - g (lista ou dicionário): grafo reprasentado por uma matriz de adjacência 
+        ou uma lista de adjacência
+        - v_1 (int): vértice inicial
+    ------------------------------------------------------------------------------
+    SAÍDA:
+        - explored (list): lista contendo os vértices do grafo na ordem em que foram
+        explorados (removidos da pilha)
+    '''
+    visited = [0] * len(g) # Lista informando quais vértices já foram explorados
+    visited[v_1-1] = 1
+    explored = []
+    P = [v_1]
+    
+    # Caso o grafo seja representado por uma lista de adjacência
+    if isinstance(g, list):
+        while P:
+            u = P.pop()
+            if u not in explored:
+                explored.append(u)
+                for i in range(len(g)-1, -1, -1):
+                    if g[u-1][i] == 1:
+                        P.append(i+1)
+
+
+    # Caso o grafo seja representado por uma lista de adjacência
+    elif isinstance(g, dict):
+        ...
+
+    return explored
+
+
+
+def genTree_DFS(g, v_1):
+    ...
+    
+
+def dist(g, v_1, v_2):
+    ...
+
+def diameter(g):
+    ...
